2225c396df97868671da3850c7d633c6153188de
Quotify HackerHouse Server <admin@quotify.it>
2011-10-17 17:48:03 -0400

    Initial Rails App Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotify::Application.config.secret_token = 'b852eb9089cf37059b1a2bdb80926a36b329dcdd2988718f409d517a194e56abe4f3cc65c0c18bb352aecb2e5c9ccd48608893a3a03a599d20d6adbf952da65a'
