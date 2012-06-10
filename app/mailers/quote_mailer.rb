483a5e199fb327fbb6aaa9c4846eccc5683bb1cf
Rob Goretsky <robert.goretsky@gmail.com>
2012-06-10 15:02:43 -0400

    Change reply email address

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -3,17 +3,18 @@ class QuoteMailer < ActionMailer::Base
   
   def quotifier_email(quote)
     @quote = quote
-    mail(:to => quote.quotifier.email, :subject => "Your Quote Has Arrived")
+    mail(:to => quote.quotifier.email, :from =>"\"Quotify.it\" <reply@quotify.it>", :subject => "Your Quote Has Arrived")
+
   end
 
   def speaker_email(quote)
     @quote = quote
-    mail(:to => quote.speaker.email, :subject => "#{quote.quotifier.name} Quotified You!")
+    mail(:to => quote.speaker.email, :from =>"\"Quotify.it\" <reply@quotify.it>", :subject => "#{quote.quotifier.name} Quotified You!")
   end
 
   def witness_email(quote, witness)
     @quote = quote
-    mail(:to => witness.email, :subject => "#{quote.quotifier.name} Included You In A Quote!")
+    mail(:to => witness.email, :from =>"\"Quotify.it\" <reply@quotify.it>", :subject => "#{quote.quotifier.name} Included You In A Quote!")
   end
 
 end
