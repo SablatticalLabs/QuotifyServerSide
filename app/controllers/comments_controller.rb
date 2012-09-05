41e408dd619d8654e7894c773217d288d56ae255
Rob Goretsky <robert.goretsky@gmail.com>
2012-09-04 23:06:57 -0400

    Allow comments to be posted via old-fashioned HTML form post as well as AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,22 @@ class CommentsController < ApplicationController
   #All methods need to get a reference to the parent quote
   before_filter { |controller| @quote = Quote.find(params[:quote_id]) }
 
- def create
-   @comment = Comment.create(params[:comment].merge(quote_id: @quote.id))
-   render :json => @comment
- end
-
+  def create 
+    @comment = Comment.new(params[:comment].merge(quote_id: @quote.id))
+  
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to quote_path(@quote), notice: 'Quote image was successfully created.' }
+        format.json { render json: @comment, status: :created }
+      else
+        format.html { redirect_to quote_path(@quote), notice: "Quote save had errors: #{@comment.errors.to_s}." }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+ 
+  def new
+    @comment = Comment.new
+  end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :quote
+  validates_presence_of :commenter_name
 end
