b22dcc5ca6211eb4e4a0d9d0864ffc7e58d72783
Rob Goretsky <robert.goretsky@gmail.com>
2012-09-18 23:53:21 -0400

    Set up scope for finding comments to email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :quote
   validates_presence_of :commenter_name
   validates_presence_of :comment_text
+  before_create :set_email_sent_flag
+
+  def set_email_sent_flag
+    self.email_sent_flag = false
+    true
+  end
 end
