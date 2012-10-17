c06df6addeb7bf9917f600f4ef3aa694a72cd018
Rob Goretsky <robert.goretsky@gmail.com>
2012-10-17 00:01:52 -0400

    Dont send email to person submitting comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,19 @@
 class Comment < ActiveRecord::Base
   belongs_to :quote
+  belongs_to :user
   validates_presence_of :commenter_name
   validates_presence_of :comment_text
 
-  def send_messages
 
-    CommentMailer.comment_email(self, self.quote.quotifier).deliver
+  def send_messages
+    #Message is not sent to the commenter himself, just to everyone else. 
+
+    CommentMailer.comment_email(self, self.quote.quotifier).deliver unless self.quote.quotifier == self.user
 
     send_erorrs = []
 
-    #If the person quotified themselves speaking, dont want to send two messages
-    unless self.quote.speaker.same_email_or_phone_as(self.quote.quotifier)
+    #If the person quotified themselves speaking, dont want to send two messages.  Also don't send message to person who left the comment.
+    unless self.quote.speaker.same_email_or_phone_as(self.quote.quotifier) or self.quote.speaker == self.user
       unless self.quote.speaker.email.blank?
         CommentMailer.comment_email(self, self.quote.speaker).deliver
       else
@@ -22,12 +25,14 @@ class Comment < ActiveRecord::Base
 
     self.quote.quote_witness_users.each do |quote_witness|
       witness  = quote_witness.witness
-      unless witness.email.blank?
-        CommentMailer.comment_email(self, witness).deliver
-      else
-       #TODO: Figure out setup for text messages
-       #send_status = send_twillio_message(witness.phone, "#{quotifier.name} Quotified you!  Check it out at http://quotify.it/#{quote_witness.witness_quote_id}")
-       #send_errors << send_status unless send_status == 'Success'
+      unless witness == self.user
+        unless witness.email.blank?
+          CommentMailer.comment_email(self, witness).deliver
+        else
+         #TODO: Figure out setup for text messages
+         #send_status = send_twillio_message(witness.phone, "#{quotifier.name} Quotified you!  Check it out at http://quotify.it/#{quote_witness.witness_quote_id}")
+         #send_errors << send_status unless send_status == 'Success'
+        end
       end
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120905003933) do
+ActiveRecord::Schema.define(:version => 20121017034553) do
 
   create_table "comments", :force => true do |t|
     t.string   "comment_text",    :null => false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120905003933) do
     t.string   "commenter_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "quote_images", :id => false, :force => true do |t|
