1c4aa6f3de5b94a271c48ae19e88c4b884d5d270
Rob Goretsky <robert.goretsky@gmail.com>
2012-11-27 23:13:27 -0500

    Allow for different text in comment emails depending on who you are

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,8 +1,9 @@
 class CommentMailer < ActionMailer::Base
   default from: "reply@quotify.it"
 
-  def comment_email(comment, to_user)
+  def comment_email(comment, to_user, role)
     @comment = comment
+    @role = role
     @personalized_quote_id = comment.quote.personalized_quote_id_for(to_user)
     mail(:to => to_user.email, :from =>"\"Quotify.it\" <reply@quotify.it>", :subject => "There's A New Comment on #{comment.quote.speaker.name}'s Quote!")
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,12 +7,12 @@ class Comment < ActiveRecord::Base
 
   def send_messages
     #Message is not sent to the commenter himself, just to everyone else. 
-    CommentMailer.comment_email(self, self.quote.quotifier).deliver unless self.quote.quotifier == self.user
+    CommentMailer.comment_email(self, self.quote.quotifier, :quotifier).deliver unless self.quote.quotifier == self.user
 
     #If the person quotified themselves speaking, dont want to send two messages.  Also don't send message to person who left the comment.
     unless self.quote.speaker.same_person_as(self.quote.quotifier) or self.quote.speaker == self.user
       unless self.quote.speaker.email.blank?
-        CommentMailer.comment_email(self, self.quote.speaker).deliver
+        CommentMailer.comment_email(self, self.quote.speaker, :speaker).deliver
       else
         QuotifyTwilio.send_twilio_message(self.quote.speaker.phone, "There's a new comment on #{self.quote.quotifier.name}'s quote!  Check it out at http://quotify.it/#{self.quote.speaker_quote_id}")
       end
@@ -22,7 +22,7 @@ class Comment < ActiveRecord::Base
       witness  = quote_witness.witness
       unless witness == self.user
         unless witness.email.blank?
-          CommentMailer.comment_email(self, witness).deliver
+          CommentMailer.comment_email(self, witness, :witness).deliver
         else
           QuotifyTwilio.send_twilio_message(witness.phone, "There's a new comment on #{self.quote.quotifier.name}'s quote!  Check it out at http://quotify.it/#{quote_witness.witness_quote_id}")  
         end
